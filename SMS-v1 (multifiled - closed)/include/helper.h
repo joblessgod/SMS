c6a6eb63472d18970cb01c8dc262e7dd8bb0fa4c
JobLessGod <iamjoblessgod@gmail.com>
2025-06-27 21:59:47 +0545

    ✅ "Basic Authentication"

--- SMS-v1 (multifiled - closed)/include/helper.h
+++
@@ -1,6 +0,0 @@
-#ifndef HELPER_H
-#define HELPER_H
-
-void takeInput(char ch[50]);
-void takePassword(char *pwd);
-#endif
--- SMS-v1 (multifiled - closed)/include/mainmenu.h
+++
@@ -1,5 +0,0 @@
-#ifndef MAINMENU_H
-#define MAINMENU_H
-
-void showMainMenu();
-#endif
